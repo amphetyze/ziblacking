684051deff2fbaf26315d19f7895cfae5cbe1408
amphetyze <115869203+amphetyze@users.noreply.github.com>
2024-06-14 20:21:55 +0800

    Add files via upload

--- js/clicker.js
+++ js/clicker.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('.hero__clicker-button');
 let result = document.getElementById('clicker-result');
+let stats = document.querySelector('.hero__clicker-stats');
 let currentCount = 0;
 
 button.addEventListener('click', updateResult);
@@ -7,4 +8,9 @@ button.addEventListener('click', updateResult);
 function updateResult() {
     currentCount += 1;
     result.textContent = currentCount;
+
+    // Trigger the animation
+    stats.style.animation = 'none'; // Reset the animation
+    void stats.offsetWidth; // Trigger a reflow to restart the animation
+    stats.style.animation = 'heartbeat .1s linear';
 }
